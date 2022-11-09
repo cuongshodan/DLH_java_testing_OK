a6efe537cc662d80c3b3a91c3c2510035ddea220
Julien Desmulier <julien.desmulier@q-leap.eu>
2022-11-09 07:35:10 +0000

    MathClass.java edited online with Bitbucket

--- src/test/java/testExercisePackage/MathClass.java
+++ src/test/java/testExercisePackage/MathClass.java
@@ -12,19 +12,8 @@ public class MathClass {
 
         // assert statements
         Assert.assertEquals("10 x 0 must be 0", 0, tester.multiply(10, 0));
-        Assert.assertEquals("0 x 10 must be 0", 0, tester.multiply(0, 10));
-        Assert.assertEquals("0 x 0 must be 0", 0, tester.multiply(0, 0));
-        Assert.assertEquals("10 x 10 must be 100", 100, tester.multiply(10, 10));
         
     }
     
-     @Test
-    public void multiplicationOfNonZeroIntegersShouldNotReturnZero() {
-        MultiplyClass tester = new MultiplyClass(); // MyClass is tested
-
-        // assert statements
-        Assert.assertEquals("10 x 10 must be 100", 100, tester.multiply(10, 10));
-        Assert.assertEquals("10 x 1 must be 10", 10, tester.multiply(10, 1));        
-    }
 }
 
